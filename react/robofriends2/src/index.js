befcf822e3fe58969391572ed699b9718f3ad18e
ZhiHanZ <568532474@qq.com>
2018-09-14 03:59:34 +0800

    react - 3

--- react/robofriends2/src/index.js
+++ react/robofriends2/src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import {robots} from './robots';
-import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id = {robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>, 
+import Cardlist from './Cardlist'
+ReactDOM.render(<Cardlist robots={robots}/>,
     document.getElementById('root'));
 registerServiceWorker();
