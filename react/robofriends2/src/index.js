b744617ce240e5a65f69b7a2421220f091e9c6b8
ZhiHanZ <568532474@qq.com>
2018-09-14 08:47:52 +0800

    react - finished-1

--- react/robofriends2/src/index.js
+++ react/robofriends2/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import {robots} from './robots';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Cardlist from './Cardlist'
-ReactDOM.render(<Cardlist robots={robots}/>,
+
+ReactDOM.render(<App/>,
     document.getElementById('root'));
 registerServiceWorker();
