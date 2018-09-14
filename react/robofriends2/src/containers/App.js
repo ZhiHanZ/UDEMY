fd442ed57acda0a2a1ba6fbe83c5b49f5c699977
ZhiHanZ <568532474@qq.com>
2018-09-14 09:53:29 +0800

    concised robofriends

--- react/robofriends2/src/containers/App.js
+++ react/robofriends2/src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Cardlist from './Cardlist';
-import SearchBox from './SearchBox';
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 class App extends Component{
     constructor(){
         super()
@@ -27,14 +27,12 @@ class App extends Component{
 
     }
     render(){
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0){
-            return <h1>loading</h1>
-        } else {
-            console.log('3');
-            return (
+            return (!robots.length)? <h1 className='f1 tc'>loading</h1>:
+        (
                 <div className='tc'>
                      <h1 className='f1'> Robofriends </h1>
                      <SearchBox searchChange={this.onSearchChange}/>;
@@ -48,5 +46,4 @@ class App extends Component{
 
     }
 
-}
 export default App;
